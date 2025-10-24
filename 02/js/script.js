7c94b2e49a45384213166a033961fff7706c5bde
FKAYSR <kejserfreja@gmail.com>
2025-10-24 13:15:44 +0200

    Opg 1

--- 02/js/script.js
+++ 02/js/script.js
@@ -2,12 +2,17 @@
 
 
 const nightColorBtn = document.getElementById("nightColorBtn");
+const mediumColorBtn = document.getElementById("mediumColorBtn");
 const lightColorBtn = document.getElementById("lightColorBtn");
 const body = document.body;
 
 
 nightColorBtn.addEventListener("click", () => {
     body.style = "background: #121212";
+})
+
+mediumColorBtn.addEventListener("click", () => {
+    body.style = "background: #777B7E";
 })
 
 lightColorBtn.addEventListener("click", () => {
