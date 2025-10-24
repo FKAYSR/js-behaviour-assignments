796f10ff51b9f9a1d83ce33c2e808f0cf8bcdb8b
FKAYSR <kejserfreja@gmail.com>
2025-10-24 13:26:22 +0200

    Update script.js

--- 03/js/script.js
+++ 03/js/script.js
@@ -6,19 +6,19 @@ const body = document.body;
 
 function updateBackground() {
 const width = window.innerWidth;
-    if (width < 640){
+    if (width < 480){
         body.style.background = "purple";
     }
     else if (width < 760){
         body.style.background = "yellow";
     }
-    else if (width < 900){
+    else if (width < 960){
         body.style.background = "blue"; 
     }
-    else if (width < 1100){
+    else if (width < 1024){
         body.style.background = "red"; 
     }
-     else if (width < 1300){
+    else if (width < 1200){
         body.style.background = "green"; 
     }
     else {
